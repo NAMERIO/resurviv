365e601a83ce62fee7122e9eb535e73c3abeba21
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-12-14 09:41:50 +0100

    bruh

--- shared/net/leaderboardMsg.ts
+++ shared/net/leaderboardMsg.ts
@@ -4,14 +4,14 @@ export class LeaderboardMsg implements AbstractMsg {
     players: { name: string; kills: number }[] = [];
 
     serialize(s: BitStream) {
-        s.writeArray(this.players, 4, (p) => {
+        s.writeArray(this.players, 16, (p) => {
             s.writeString(p.name, Constants.PlayerNameMaxLen);
             s.writeUint8(p.kills);
         });
     }
 
     deserialize(s: BitStream) {
-        this.players = s.readArray(4, () => {
+        this.players = s.readArray(16, () => {
             return {
                 name: s.readString(Constants.PlayerNameMaxLen),
                 kills: s.readUint8(),
