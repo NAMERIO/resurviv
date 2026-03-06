8dfec2747930770d197f0fd36853b0f91fb77bca
NAMERIO <natanberhanu22@gmail.com>
2026-03-06 15:30:49 -0600

    update client theme and map to inferno

--- server/src/config.ts
+++ server/src/config.ts
@@ -11,10 +11,10 @@ export const Config = getConfig(isProd, serverConfigPath);
 const BACKPACK_LEVEL = 3;
 
 util.mergeDeep(Config, {
-    clientTheme: "main",
+    clientTheme: "inferno",
     modes: [
         { mapName: "snow", teamMode: TeamMode.Solo, enabled: false },
-        { mapName: "main", teamMode: TeamMode.Duo, enabled: true },
+        { mapName: "inferno", teamMode: TeamMode.Duo, enabled: true },
         { mapName: "snow", teamMode: TeamMode.Squad, enabled: false },
     ],
     debug: {
