7afe1cb2ceaffd96927482bed1ef7fe3592f5a6a
atom9384 <watlee93@gmail.com>
2026-08-18 01:44:10 -0400

    Update buildings.ts
    Fix floor not loading

--- client/atlas-builder/defs/buildings.ts
+++ client/atlas-builder/defs/buildings.ts
@@ -327,9 +327,9 @@ export const BuildingSprites = {
         "map/map-bunker-chrys-compartment-ceiling-03a.svg",
         "map/map-bunker-chrys-compartment-ceiling-03b.svg",
         "map/map-bunker-chrys-compartment-floor-01a.svg",
-        "map/map-bunker-chrys-compartment-floor-01c.svg",
+        "map/map-bunker-chrys-compartment-floor-01b.svg",
         "map/map-bunker-chrys-compartment-floor-02a.svg",
-        "map/map-bunker-chrys-compartment-floor-02c.svg",
+        "map/map-bunker-chrys-compartment-floor-02b.svg",
         "map/map-bunker-chrys-compartment-floor-03a.svg",
     ],
 
