cc27ddf5c4fb64e5e14b160025a9540e51088497
Adan Wang <watlee93@gmail.com>
2026-08-20 01:55:18 -0400

    Fix streak perk pickup bug

--- shared/defs/gameObjects/damageStreakDefs.ts
+++ shared/defs/gameObjects/damageStreakDefs.ts
@@ -20,8 +20,8 @@ export interface DamageStreakDef {
 }
 
 export const StreakThresholds = {
-    firstActivation: 0,
-    increment: 0,
+    firstActivation: 300,
+    increment: 400,
     get(activationIndex: number): number {
         return (
             StreakThresholds.firstActivation +
