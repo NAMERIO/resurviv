7215b9cb10bfb70675c36e6d8558694db1ad36af
NAMERIO <natanberhanu22@gmail.com>
2026-08-20 21:15:31 -0500

    inferno

--- server/src/config.ts
+++ server/src/config.ts
@@ -11,9 +11,9 @@ export const Config = getConfig(isProd, serverConfigPath);
 const BACKPACK_LEVEL = 3;
 
 util.mergeDeep(Config, {
-    clientTheme: "desert",
-    modes: [{ mapName: "desert", teamMode: TeamMode.Duo, enabled: true }],
-    br_modes: [{ mapName: "br_desert", teamMode: TeamMode.Duo, enabled: true }],
+    clientTheme: "inferno",
+    modes: [{ mapName: "inferno", teamMode: TeamMode.Duo, enabled: true }],
+    br_modes: [{ mapName: "main", teamMode: TeamMode.Duo, enabled: true }],
     debug: {
         spawnMode: process.env.NODE_ENV === "production" ? "default" : "fixed",
     },
