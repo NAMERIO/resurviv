5586b82da868d6c03733510e7e405e8a60895191
Adan Wang <watlee93@gmail.com>
2026-09-13 16:22:30 -0400

    lint again

--- server/src/game/game.ts
+++ server/src/game/game.ts
@@ -780,7 +780,8 @@ export class Game {
         if (removeBattleRoyalePlayer) {
             player.game.playerBarn.removePlayer(player);
         } else if (!keepBattleRoyaleBody) {
-            if (!player.dead &&
+            if (
+                !player.dead &&
                 player.health < 50 &&
                 player.lastDamagedBy &&
                 player.disconnectTime == -1
